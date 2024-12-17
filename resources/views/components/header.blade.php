410eb1039f1cbd0e4983524da7395bc80cd31a7e
Marin Milotić <mGitM101@gmail.com>
2024-12-17 12:01:33 +0100

    38:Created a nav link component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,17 +4,11 @@
         <a href="{{url('/')}}">Workopia</a>
       </h1>
       <nav class="hidden md:flex items-center space-x-4">
-        <a href="{{url('/jobs')}}" class="text-white hover:underline py-2">All Jobs</a>
-        <a href="{{url('/jobs/saved')}}" class="text-white hover:underline py-2"
-          >Saved Jobs</a
-        >
-        <a href="{{url('/login')}}" class="text-white hover:underline py-2">Login</a>
-        <a href="{{url('/register')}}" class="text-white hover:underline py-2"
-          >Register</a
-        >
-        <a href="{{url('/dashboard')}}" class="text-white hover:underline py-2">
-          <i class="fa fa-gauge mr-1"></i> Dashboard
-        </a>
+        <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>
+        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
+        <x-nav-link url="/login" :active="request()->is('login')">Login</x-nav-link>
+        <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
+        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
         <a
           href="{{url('/jobs/create')}}"
           class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"
