dd58c38c4df28213d7a6745a95fb9bc7f827c4cb
Marin Milotić <mGitM101@gmail.com>
2025-01-15 15:30:57 +0100

    50:DB connected, changed registratin function in AppSeviceProvider for migratin some sanctum script

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,15 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Laravel\Sanctum\Sanctum;
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        Sanctum::ignoreMigrations();
     }
 
     /**
