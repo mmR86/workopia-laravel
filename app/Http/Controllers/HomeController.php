c294ddf8d3170a2691b79ef2dc421fa8a1fc6221
Marin Milotić <mGitM101@gmail.com>
2025-02-22 13:22:54 +0100

    68:Created a Card component for jobs and implemented it on HP and jobs page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
+
+
 
 class HomeController extends Controller
 {
-    public function index() {
-        return view('pages.index');
+    public function index(): View 
+    {
+        $jobs = Job::latest()->limit(6)->get();
+
+        //with koristimo da bi poslali joboe kao prop izgleda
+        return view('pages.index')->with('jobs', $jobs);
     }
 }
