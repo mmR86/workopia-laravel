4f62c8dac77701d06d43d9f73ee45ec79f628dd7
Marin Milotić <mGitM101@gmail.com>
2025-03-01 12:49:02 +0100

    79:Created update, edit and delete functionalities

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -7,6 +7,7 @@ use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 use App\Models\Job;
 use Illuminate\Contracts\Support\ValidatedData;
+use Illuminate\Support\Facades\Storage;
 
 class JobController extends Controller
 {
@@ -84,24 +85,68 @@ class JobController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Job $job): View
     {
-        //
+        return view('jobs.edit')->with('job', $job);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Job $job)
     {
-        //
+        // Validate the incoming request data
+    $validatedData = $request->validate([
+        'title' => 'required|string|max:255',
+        'description' => 'required|string',
+        'salary' => 'required|integer',
+        'tags' => 'nullable|string',
+        'job_type' => 'required|string',
+        'remote' => 'required|boolean',
+        'requirements' => 'nullable|string',
+        'benefits' => 'nullable|string',
+        'address' => 'nullable|string',
+        'city' => 'required|string',
+        'state' => 'required|string',
+        'zipcode' => 'required|string',
+        'contact_email' => 'required|email',
+        'contact_phone' => 'nullable|string',
+        'company_name' => 'required|string',
+        'company_description' => 'nullable|string',
+        'company_logo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        'company_website' => 'nullable|url',
+    ]);
+
+    //Check for image
+    if($request->hasFile('company_logo')) {
+
+        //Delete old logo
+        Storage::delete('public/logos' . basename($job->company_logo));
+        //Store the file and get path
+        $path = $request->file('company_logo')->store('logos', 'public');
+
+        //Add path to validated data (add path to DB)
+        $validatedData['company_logo'] = $path;
+    }
+
+    //Submit to DB
+    $job->update($validatedData);
+
+    return redirect()->route('jobs.index')->with('success', 'Job listing updated successfully!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Job $job): RedirectResponse
     {
-        //
+        // If logo, then delete it
+        if($job->company_logo) {
+            Storage::delete('public/logos' . basename($job->company_logo));
+        }
+
+        $job->delete();
+
+        return redirect()->route('jobs.index')->with('success', 'Job listing deleted successfully!');
     }
 }
