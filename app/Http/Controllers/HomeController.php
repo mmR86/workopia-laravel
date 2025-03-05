8bd41ccf6e55ce4e09325a024f133474a74d54b5
Marin Milotić <mGitM101@gmail.com>
2025-03-05 09:52:04 +0100

    84:Changed config files for session storage and created 2 new controllers (Login & Register)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    // @desc Show home index view
+    // @route GET /
     public function index(): View 
     {
         $jobs = Job::latest()->limit(6)->get();
