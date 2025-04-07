a3a673a255a55cf5e596e814fa4a5b2be0d94a5a
Marin Milotić <mGitM101@gmail.com>
2025-04-07 10:44:46 +0200

    101:Added a users profile photo in nav, when logged in

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,9 +7,17 @@
         <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>
         @auth
         <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
-        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
         <x-logout-button />
         <x-button-link url='/jobs/create' icon='edit'>Create Job</x-button-link>
+        <div class="flex items-center space-x-3">
+          <a href="{{route('dashboard')}}">
+            @if(Auth::user()->avatar)
+              <img src="{{asset('storage/' . Auth::user()->avatar)}}" alt="{{Auth::user()->name}}" class="w-10 h-10 rounded-full">
+            @else
+              <img src="{{asset('storage/avatars/default-avatar.png')}}" alt="{{Auth::user()->name}}" class="w-10 h-10 rounded-full">
+            @endif
+          </a>
+        </div>
         @else
         <x-nav-link url="/login" :active="request()->is('login')">Login</x-nav-link>
         <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
